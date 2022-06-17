c32424487d5eb7b9b187aefd9dcaafb5f5f0f7cb
DimDob <102876245+DimDob@users.noreply.github.com>
2022-06-18 00:20:38 +0300

    Web

--- Project/app.js
+++ Project/app.js
@@ -7,9 +7,22 @@ menu.addEventListener('click', function(){
 })
 
 
-function myFunction() {
-    window.location.href="products.html";
+
+
+
+let registerBtn = document.getElementsByClassName('registerbtn')[0]
+function trigger() {
+    let passwordInput = document.getElementById('psw').value
+    let emailInput = document.getElementById('email').value
+    const result = emailInput.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'g')
+    const strongPasswordRegExp = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
+
+    if (result == null){
+        alert('Wrong email! Please select a valid email.')
+    } else if (strongPasswordRegExp.test(passwordInput) == false) {
+        alert('Please enter more secure password!')
+    } else { 
+        alert('Welcome! Your data has been registered to the system.')
+        window.location.href = 'main_page.html'
+    }
 }
-
-
-
